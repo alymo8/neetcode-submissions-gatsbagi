ebc6b6fadfe3fdde7d8e1cf7de0e4e70298fdf63
Aly Mohamed Hussein <aly.mohamed@mail.mcgill.ca>
2026-04-21 14:10:16 -0400

    Add: pascals-triangle - submission-0

---
+++ Data Structures & Algorithms/pascals-triangle/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = [[1]]
+        for i in range(2,numRows+1):
+            res.append([0] * i)
+        
+        for i in range(1,len(res)):
+            for j in range(len(res[i])):
+                if j-1 >= 0:
+                    upleft = res[i-1][j-1]
+                else:
+                    upleft = 0
+                if j < len(res[i-1]):
+                    upright = res[i-1][j]
+                else:
+                    upright = 0
+                
+                res[i][j] = upleft + upright
+            # res[i][-1] = 1
+
+        return res
+
+
+
+
+
+
+        
