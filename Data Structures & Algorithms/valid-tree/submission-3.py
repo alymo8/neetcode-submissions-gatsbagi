49a27e8298adc0ab6d589acc974e3cd564d24522
Aly Mohamed Hussein <aly.mohamed@mail.mcgill.ca>
2026-05-01 17:27:12 -0400

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        
+        adj_map = {i:[] for i in range(n)}
+        for u,v in edges:
+            adj_map.get(u,[]).append(v)
+            adj_map.get(v,[]).append(u)
+        seen = set()
+
+        def traverse(node, parent):
+            if node in seen:
+                return False
+            seen.add(node)
+            for neighbor in adj_map[node]:
+                if neighbor == parent:
+                    continue
+                if not traverse(neighbor, node):
+                    return False
+            
+            return True
+
+        if not traverse(0, None) or len(seen) != n:
+            return False
+        return True
