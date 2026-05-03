c0116d75efdce7b4ab46e0e331d30976a9bd4e6c
Aly Mohamed Hussein <aly.mohamed@mail.mcgill.ca>
2026-05-03 12:14:37 -0400

    Add: search-for-word - submission-3

---
+++ Data Structures & Algorithms/search-for-word/submission-3.py
@@ -0,0 +1,36 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+
+        if len(word) == 1:
+            return any(word[0] in row for row in board)
+            
+        n = len(board)
+        m = len(board[0])
+
+
+        def check_next_char(i, j, k, visited):
+            if k >= len(word):
+                return False
+            neighbors = [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]
+            # found_char = False
+            for ni, nj in neighbors:
+                if 0 <= ni < n and 0 <= nj < m and (ni,nj) not in visited:
+                    visited.add((ni,nj))
+                    if board[ni][nj] == word[k] and k == len(word) - 1:
+                        return True
+                    if board[ni][nj] == word[k] and check_next_char(ni, nj, k+1, visited):
+                        return True
+                    visited.remove((ni,nj))
+            return False
+
+        k = 0
+
+        for i in range(n):
+            for j in range(m):
+                c = board[i][j]
+                if c == word[k]:
+                    if check_next_char(i, j, k + 1, {(i,j)}):
+                        return True
+        return False
+
+        
