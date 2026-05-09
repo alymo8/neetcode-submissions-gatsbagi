c1088c26bee3e9f1e522680654f994ca9eeefe20
Aly Mohamed Hussein <aly.mohamed@mail.mcgill.ca>
2026-05-08 23:45:29 -0400

    Add: find-k-closest-elements - submission-2

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-2.py
@@ -0,0 +1,23 @@
+import heapq
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        # define array res of size k, start with empty
+        # iterate on arr and start populating until size k reached
+        # if size = k, compare abs differenc - new value with first index, 
+        #    if smaller pop index 0 and add to end of arr, 
+        #    keep going until we're not modifying res
+
+        res = []
+        for val in arr:
+            if len(res) < k:
+                res.append(val)
+            else:
+                if abs(res[0] - x) > abs(val - x):
+                    res.pop(0)
+                    res.append(val)
+                
+        return res
+        
+
+
+            
